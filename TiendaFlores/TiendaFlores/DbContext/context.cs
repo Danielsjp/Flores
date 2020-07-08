117f9311eed157346716c20524ecb61b10f96f6a
Dani Sán <40555221+Danielsjp@users.noreply.github.com>
2020-07-08 10:26:38 +0200

    ok
    ok

---
+++ TiendaFlores/TiendaFlores/DbContext/context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaFlores.Class;
+namespace TiendaFlores.DbContext
+{
+    public class context
+    {
+        public static Dictionary<Guid, String> tiendas = new Dictionary<Guid, String>();
+        public static Dictionary<Guid, Flor> flor = new Dictionary<Guid, Flor>();
+        public static Dictionary<Guid, Decorations> decorations = new Dictionary<Guid, Decorations>();
+        public static Dictionary<Guid, Arbol> arbol = new Dictionary<Guid, Arbol>();
+    }
+}
---
+++ TiendaFlores/TiendaFlores/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TiendaFlores.Class;
+using TiendaFlores.DbContext;
+
+namespace TiendaFlores
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+           
+    public partial class MainWindow : Window
+    {       
+        public MainWindow()
+        {
+        InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+                var flores = new Floristeria
+                { 
+                Id = Guid.NewGuid(),
+                Name = FlorisName.Text
+                };
+                if (flores.Check1(flores.Name))
+                {
+                DbContext.context.tiendas.Add(flores.Id, flores.Name);
+                var arboles = new Arbol
+                {
+                    Id = Guid.NewGuid(),
+                    Name = FlorisName.Text,
+                    Height = Int32.Parse(TreeSize.Text),
+                    Price = Int32.Parse(TreePrice.Text)
+                };
+                DbContext.context.arbol.Add(arboles.Id, arboles);
+                var flor = new Flor
+                {
+                    Id = Guid.NewGuid(),
+                    Name = FlorisName.Text,
+                    Color = FlowerColor.Text,
+                    Price = Int32.Parse(FlowerPrice.Text)
+
+                };
+                DbContext.context.flor.Add(flor.Id, flor);
+                var decoration = new Decorations
+                {
+                    Id = Guid.NewGuid(),
+                    Name = FlorisName.Text,
+                    Material = Decoration.Text,
+                    Price = Int32.Parse(DecoPrice.Text)
+                };
+                DbContext.context.decorations.Add(decoration.Id, decoration);
+                int TiendasCount = DbContext.context.tiendas.Count;
+                int suma = 0;
+                foreach (var i in DbContext.context.arbol.Values)
+                {
+                    suma = suma + i.Price;
+                }
+                NumTiendas.Content = TiendasCount;
+                TotalEuros.Content = suma;
+            }
+            else { MessageBox.Show("Ya existe una tienda con ese nommbre"); }           
+        }
+    }       
+}
+
